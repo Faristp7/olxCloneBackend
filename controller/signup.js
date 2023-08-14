7b5e75ba206c46c2042e5f01b642e8dcc1bfc1db
faris <mrfaristp@gmail.com>
2023-08-14 16:16:27 +0530

    logni added

--- controller/signup.js
+++ controller/signup.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import userModel from "../model/userModel.js";
+import jwt from "jsonwebtoken";
 
 var salt = bcrypt.genSaltSync(10);
 
@@ -13,8 +14,50 @@ export async function userRegister(req, res) {
     } else {
       const newUser = new userModel({ name, email, password: hashedPassword });
       await newUser.save();
+      res.send(true);
     }
   } catch (error) {
     console.log(error);
   }
 }
+
+export async function login(req, res) {
+  try {
+    const { loginEmail, loginPassword } = req.body;
+    if (loginEmail && loginPassword) {
+      const user = await userModel.find({ email: loginEmail });
+      const userValid = bcrypt.compareSync(loginPassword, user[0].password);
+      if (user && userValid) {
+        const token = jwt.sign({ id: user[0]._id }, "myKey");
+        res.cookie("jwtToken", token, {
+          httpOnly: true,
+          secure: true,
+          maxAge: 3600000, //1hour
+          sameSite: "none",
+        });
+        res.json({ message: true });
+      } else {
+        res.send(false);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+export const checkUserLoggedIn = async (req, res) => {
+  try {
+    const token = req.cookies.jwtToken;
+    if (!token)
+      return res.json({ loggedIn: false, error: true, message: "no token" });
+
+    const verifiedJWT = jwt.verify(token, "myKey");
+    const user = await userModel.findById(verifiedJWT.id, { password: 0 });
+    if (!user) {
+      return res.json({ loggedIn: false });
+    }
+    return res.json({ user, loggedIn: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ loggedIn: false, error: err });
+  }
+};
--- router/olxRouter.js
+++ router/olxRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
 import multer from "multer";
-import { userRegister } from "../controller/signup.js";
+import {
+  checkUserLoggedIn,
+  login,
+  userRegister,
+} from "../controller/signup.js";
 
 const router = express.Router();
 
-router.get('/')
-router.post('/register', userRegister)
+router.get("/");
+router.post("/register", userRegister);
+router.post("/login", login);
+router.get("/login/check", checkUserLoggedIn);
 
-export default router
+export default router;
